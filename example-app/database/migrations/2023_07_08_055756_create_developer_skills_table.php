79e5e83dbdb2bfa1d809f51d3caf1bb7645d11cb
Naimur <naimurshuvo.bd.16@gmail.com>
2023-07-08 12:08:14 +0600

    Developer Skills Table Added

--- example-app/database/migrations/2023_07_08_055756_create_developer_skills_table.php
+++ example-app/database/migrations/2023_07_08_055756_create_developer_skills_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('developer_skills', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("Skill_name");
+            $table->string("Skill_set");
+
+
         });
     }
 
